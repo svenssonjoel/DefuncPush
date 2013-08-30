e3805f7d85cb2b28c96f1d60d019be2c924c182d
Joel Svensson <svenssonjoel@yahoo.se>
2013-08-30 18:10:55 +0200

    additions

--- hs/Push.hs
+++ hs/Push.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Push where
 
@@ -17,6 +19,12 @@ type Length = Int
 
 
 ---------------------------------------------------------------------------
+-- Pull array
+---------------------------------------------------------------------------
+
+data Pull a = Pull (Index -> a) Length 
+
+---------------------------------------------------------------------------
 -- Push array
 --------------------------------------------------------------------------- 
 data Push m a = Push ((Index -> a -> m ()) -> m ()) Length 
@@ -26,6 +34,13 @@ data Push m a = Push ((Index -> a -> m ()) -> m ()) Length
 ---------------------------------------------------------------------------
 -- Basic functions on push arrays
 ---------------------------------------------------------------------------
+
+len :: Push m a -> Length
+len (Push _ n) = n
+
+(<:) :: Push m a -> (Index -> a -> m ()) -> m () 
+(Push p _) <: k = p k 
+
 map :: (a -> b) -> Push m a -> Push m b
 map f (Push p l) = Push (\k -> p (\i a -> k i (f a))) l
 
@@ -35,11 +50,31 @@ imap f (Push p l) = Push (\k -> p (\i a -> k i (f a i))) l
 ixmap :: (Index -> Index) -> Push m a -> Push m a
 ixmap f (Push p l) = Push (\k -> p (\i a -> k (f i) a)) l 
 
-(++) :: Monad m => Push m a -> Push m a  -> Push m a
-(Push f l1) ++ (Push g l2) =
-  Push (\k -> f k >> g (\i a -> k (l1 + i) a)) (l1 + l2) 
+(++) :: Monad m =>  Push m a -> Push m a  -> Push m a
+p1 ++ p2 = 
+  Push (\k -> p1 <: k >>
+              p2 <: (\i a -> k (len p1 + i) a)
+       ) (len p1 + len p2) 
 
+reverse :: Push m a -> Push m a
+reverse p = ixmap (\i -> (len p - 1) - i) p
 
+---------------------------------------------------------------------------
+-- Conversion Pull Push
+---------------------------------------------------------------------------
+
+push (Pull ixf n) =
+  Push (\k -> forM_ [0..(n-1)] $ \i ->
+         k i (ixf i)) n 
+
+class ToPush m arr where
+  toPush ::  arr a -> Push m a
+
+instance Monad m => ToPush m (Push m) where
+  toPush = id
+
+instance Monad m => ToPush m Pull where
+  toPush = push 
 
 
 ---------------------------------------------------------------------------
@@ -54,3 +89,5 @@ freeze (Push p l) =
      V.freeze arr 
 
 
+---------------------------------------------------------------------------
+-- 
